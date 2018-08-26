235b5c39c8967b4731103582c82622e5f9b8e529
Kutub Gandhi <kksgandhi@gmail.com>
2018-08-26 09:36:23 -0500

    deleted unneccessary code

--- app.js
+++ app.js
@@ -14,37 +14,12 @@ const client = new Discord.Client();
 // On ready
 client.on('ready', () => {
     console.log('Ready.');
-    //client.user.setGame('https://github.com/Moquo/discord-reaction-bot')
 });
 
 // On message
 client.on('message', (msg) => {
-    // Is the sender a bot? If it is, cancel
-    if(msg.author.bot) return;
-
-    // Check if information command was called
-		/*
-    if(msg.content === 'reaction-bot') {
-        msg.channel.send({
-            embed: {
-                color: 0x37dbd0,
-                author: {
-                    name: client.user.name,
-                    icon_url: client.user.avatarURL
-                },
-                title: 'Reaction bot by Moquo',
-                url: 'https://github.com/Moquo/discord-reaction-bot',
-                description: 'Reaction bot adds reactions to messages'
-            }
-        });
-        return;
-    }
-		*/
-
-    // React to the message
-    //if(config.rankCheck.enabled && !(msg.member.roles.some(role => config.rankCheck.roles.includes(role.name)))) return;
-    //msg.react(config.react_emoji);
     msg.react("480854406263472138");
+    msg.react("💩");
 });
 
 // Login with bot token
